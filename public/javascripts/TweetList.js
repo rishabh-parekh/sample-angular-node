7381a7340925a48804ae66fd9af58565fd36f233
Jonathan Cipriano <jcipriano@twitter.com>
2014-07-28 13:05:51 -0700

    Implementing oEmbed tweets.

--- public/javascripts/TweetList.js
+++ public/javascripts/TweetList.js
@@ -9,7 +9,7 @@ app.controller('TweetList', function($scope, $resource, $timeout) {
 
       // initiate masonry
       $scope.msnry = new Masonry('#tweet-list', {
-        columnWidth: 295,
+        columnWidth: 320,
         itemSelector: '.tweet-item',
         transitionDuration: 0,
         isFitWidth: true
@@ -20,6 +20,12 @@ app.controller('TweetList', function($scope, $resource, $timeout) {
       
       // empty tweet model
       $scope.tweetsResult = [];
+
+      // layout masonry.js on widgets.js loaded event
+      twttr.events.bind('loaded', function () {
+        $scope.msnry.reloadItems();
+        $scope.msnry.layout();
+      });
 
       $scope.getTweets();
     }
@@ -48,70 +54,16 @@ app.controller('TweetList', function($scope, $resource, $timeout) {
           $scope.tweetsResult = [];
         }
 
-        angular.forEach(res, function(tweet, key) {
-          tweet.created_at = processTime(tweet.created_at);
-          tweet.photos = processPhotos(tweet.entities.media);
-        });
-
         $scope.tweetsResult = $scope.tweetsResult.concat(res);
 
         // for paging - https://dev.twitter.com/docs/working-with-timelines
         $scope.maxId = res[res.length - 1].id;
 
-        // apply masonry layout
+        // render tweets with widgets.js
         $timeout(function () {
-          $scope.msnry.reloadItems();
-          $scope.msnry.layout();
+          twttr.widgets.load();
         }, 30);
       });
-    }
-
-    /**
-     * change the date format with moment.js per Twitter display requirements
-     */
-    function processTime (dateStr) {
-
-      var twtDate = moment( new Date(dateStr) ),
-      now = new Date();
-
-      if (twtDate.diff(now, 'hours') > -24) {
-        // less than 24 hours ago - how long ago was it posted?
-        dateStr = twtDate.fromNow()
-      } else {
-        // more than 24 hours ago - just the date/time
-        dateStr = twtDate.format('MMM D, YYYY h:mm a');
-      }
-
-      return dateStr;
-    }
-
-    /**
-     * calculates image sizes so masonry can layout correctly
-     */
-    function processPhotos (media) {
-      
-      var photos = [],
-      IMAGE_WIDTH = 243,
-      scale;
-
-      if( angular.isDefined(media) ) {
-        angular.forEach(media, function(mediaItem, key) {
-          if( mediaItem.type == 'photo' ) {
-
-            // the API return images sizes so we can calculate proper image size without loading the image
-            scale = IMAGE_WIDTH / mediaItem.sizes.small.w; 
-
-            photos.push({
-              url: mediaItem.media_url + ':small',
-              permalink: mediaItem.url,
-              width: IMAGE_WIDTH,
-              height: Math.floor(mediaItem.sizes.small.h * scale)
-            });
-          }
-        });
-      }
-
-      return photos;
     }
 
     /**
--- routes/tweets.js
+++ routes/tweets.js
@@ -2,44 +2,72 @@ var express = require('express');
 var router = express.Router();
 var tweetText = require('twitter-text');
 var Twit = require('twit')
+var request = require('request');
 var config = require('../config');
 
-// instantiate Twitter module
-//var twitter = new TwitterClient(config.twitter);
+// instantiate Twit module
+var twitter = new Twit(config.twitter);
 
-var twit = new Twit(config.twitter);
+var TWEET_COUNT = 5;
+var MAX_WIDTH = 305;
+var OEMBED_URL = 'https://api.twitter.com/1/statuses/oembed.json';
+var USER_TIMELINE_URL = '/statuses/user_timeline';
 
 /* GET tweets json. */
 router.get('/user_timeline/:user', function(req, res) {
 
-  function onComplete (err, data, resp) {
-  	res.setHeader('Content-Type', 'application/json');
+  var oEmbedTweets = [], tweets = [],
 
-    var twtText, tweets = data,
-  	i = 0, len = tweets.length;
-
-    for(i; i < len; i++) {
-      // add links to tweet entities using Twitter's text processing library
-      // https://github.com/twitter/twitter-text-js
-      tweets[i].text = tweetText.autoLink(tweets[i].text);
-    }
-
-    res.send(tweets);
-  };
-
-  var params = {
+  params = {
     screen_name: req.params.user, // the user id passed in as part of the route
-    count: 30 // how many tweets to return
+    count: TWEET_COUNT // how many tweets to return
   };
 
-  // the max_id is passed in via a query string param:
-  // https://dev.twitter.com/docs/working-with-timelines
+  // the max_id is passed in via a query string param
   if(req.query.max_id) {
     params.max_id = req.query.max_id;
   }
 
   // request data 
-  twit.get('/statuses/user_timeline', params, onComplete);
+  twitter.get(USER_TIMELINE_URL, params, function (err, data, resp) {
+    res.setHeader('Content-Type', 'application/json');
+
+    tweets = data;
+
+    var i = 0, len = tweets.length;
+
+    for(i; i < len; i++) {
+      getOEmbed(tweets[i]);
+    }
+  });
+
+  /**
+   * requests the oEmbed html
+   */
+  function getOEmbed (tweet) {
+
+    // oEmbed request params
+    var query = {
+      "id": tweet.id_str,
+      "url": 'https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id,
+      "maxwidth": MAX_WIDTH,
+      "hide_thread": true,
+      "omit_script": true
+    };
+
+    // oEmbed request
+    request.get(OEMBED_URL, { 'qs': query }, function (err, data, resp) {
+    
+      var data = JSON.parse(resp);
+
+      tweet.oEmbed = data;
+      oEmbedTweets.push(tweet);
+
+      if (oEmbedTweets.length == tweets.length) {
+        res.send(oEmbedTweets);
+      }
+    });
+  }
 });
 
 module.exports = router;
